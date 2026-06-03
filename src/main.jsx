606adc176516b77151cac8f92bf41aea43baa6fb
Yire <yire2005lol@gmail.com>
2026-06-03 13:14:01 -0500

    cambios 2

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './styles/indexs.css'
-import Index from './pages/Index.jsx'
+import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Index />
+    <App />
   </StrictMode>,
 )
