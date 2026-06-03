1d373a1b4ee6535be61b3e8607e4b2bd9457418f
Jair <jairturinparedes27@gmail.com>
2026-06-03 14:29:16 -0500

    actuqalizacion de contacto y footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import './App.css'
 import Navbar from './components/Navbar'
+import Footer from './components/Footer'
+import Contacto from './components/Contacto'
 
 function App() {
   return (
     <>
       <Navbar />
+      <Contacto />
+      <Footer />
     </>
   )
 }
