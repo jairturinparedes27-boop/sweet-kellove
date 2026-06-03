c1138ed998dae1d31d877c224137023c6545e97c
Jair <jairturinparedes27@gmail.com>
2026-06-03 17:19:25 -0500

    cracion de sobr nosotros

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Navbar from './components/Navbar'
 import Footer from './components/Footer'
 import Contacto from './components/Contacto'
 import Products from './components/Products'
+import SobreNosotros from './components/Sobrenosotros'
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Navbar />
       <Hero />
       <Products />
+      <SobreNosotros />
       <Contacto />
       <Footer />
     </>
